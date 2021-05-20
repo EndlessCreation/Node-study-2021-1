6d6044096a819dd7194a4235b995c858df00f53b
오예진 <tnska0421@naver.com>
2021-05-20 16:59:24 +0900

    fix todo

--- yejin/learn-sequelize/app.js
+++ yejin/learn-sequelize/app.js
@@ -8,7 +8,7 @@ const { sequelize } = require('./models');
 const app = express();
 app.set('port', process.env.PORT || 3001);
 
-sequelize.sync({ force: false })
+sequelize.sync({ force: true })
   .then(() => {
     console.log('데이터베이스 연결 성공');
   })
--- yejin/learn-sequelize/routes/todos.js
+++ yejin/learn-sequelize/routes/todos.js
@@ -14,25 +14,28 @@ router.get("/", async (req, res) => {
            status: 200,
            success: true,
            message: "todolist 조회 성공",
-           data: data,
+           data: data
         });
      })
-     .catch(() => {
+     .catch((err) => {
         res.send({
            status: 500,
            success: false,
-           message: "todolist 조회 실패",
+           message: err,
         });
      });
 });
 
-  router.post(async (req, res, next) => {
+  router.post("/", async (req, res, next) => {
+    console.log(req.body);
     try {
+  
       const todos = await Todo.create({
         todo_content: req.body.todo_content,
         todo_status: req.body.todo_status,
         todo_importance: req.body.todo_importance,
       });
+      
       console.log(todos);
       res.status(201).json(todos);
     } catch (err) {
